ef082e809c3be104a1496a85b6dfa8415da71133
DeathG7n <christariccykid55@gmail.com>
2026-06-21 06:47:06 +0100

    fixed problem

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,39 +1,41 @@
-import { logoutAccount } from '@/lib/actions/user.actions'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import { logoutAccount } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
 
-const Footer = ({ user, type = 'desktop' }: FooterProps) => {
+const Footer = ({ user, type = "desktop" }: FooterProps) => {
   const router = useRouter();
 
   const handleLogOut = async () => {
     const loggedOut = await logoutAccount();
 
-    if(loggedOut) router.push('/sign-in') 
-  }
+    if (loggedOut) router.push("/sign-in");
+  };
 
   return (
     <footer className="footer mt-5">
-      <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
+      <div className={type === "mobile" ? "footer_name-mobile" : "footer_name"}>
         <p className="text-10 font-bold text-gray-700">
-          {user?.firstName[0]}
+          {user?.firstName?.[0]}
         </p>
       </div>
 
-      <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
-          <h1 className="text-10 truncate text-gray-700 font-semibold">
-            {user?.firstName}
-          </h1>
-          <p className="text-10 truncate font-normal text-gray-600">
-            {user?.email}
-          </p>
+      <div
+        className={type === "mobile" ? "footer_email-mobile" : "footer_email"}
+      >
+        <h1 className="text-10 truncate text-gray-700 font-semibold">
+          {user?.firstName}
+        </h1>
+        <p className="text-10 truncate font-normal text-gray-600">
+          {user?.email}
+        </p>
       </div>
 
       <div className="footer_image" onClick={handleLogOut}>
         <Image src="icons/logout.svg" fill alt="jsm" />
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
